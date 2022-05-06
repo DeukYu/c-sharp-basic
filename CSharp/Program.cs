b5d55f18d17f2f671d3eb9b8b77cda7e29849963
DeukYu <ldy8070@naver.com>
2022-05-06 18:18:14 +0900

    가위바위보 게임
    가위바위보 게임

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -6,7 +6,34 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rand = new Random();
+            int aiChoice = rand.Next(0, 3);
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("컴퓨터의 선택은 가위입니다. ");
+                    break;
+                case 1:
+                    Console.WriteLine("컴퓨터의 선택은 바위입니다. ");
+                    break;
+                case 2:
+                    Console.WriteLine("컴퓨터의 선택은 보입니다. ");
+                    break;
+            }
+
+            // 승리 무승부 패배
+            if (choice == aiChoice)
+                Console.WriteLine("무승부");
+            else
+            {
+                if ((aiChoice == 0 && choice == 1) || (aiChoice == 1 && choice == 2) || (aiChoice == 2 && choice == 0))
+                    Console.WriteLine("승리");
+                else
+                    Console.WriteLine("패배");
+            }
         }
     }
 }
