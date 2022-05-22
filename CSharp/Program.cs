f5091d45d3d163e9688eb07998e5f84d2f8de1d7
DeukYu <ldy8070@naver.com>
2022-05-22 14:52:33 +0900

    다차원 배열
    다차원 배열

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,14 +4,39 @@ namespace CSharp // Note: actual namespace depends on the project name.
 {
     class Program
     {
+        class Map
+        {
+            int[,] titles =
+            {
+                {1, 1,1,1, 1 },
+                {1, 0,0,0, 1 },
+                {1, 0,0,0, 1 },
+                {1, 0,0,0, 1 },
+                {1, 1,1,1, 1 }
+            };
+
+            public void Render()
+            {
+                ConsoleColor defaultColor = Console.ForegroundColor;
+                for(int i = 0; i < titles.GetLength(1); i++)
+                {
+                    for(int j = 0; j < titles.GetLength(0); j++)
+                    {
+                        if (titles[j, i] == 1)
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        else
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write('\u25cf');
+                    }
+                    Console.WriteLine();
+                }
+                Console.ForegroundColor = defaultColor;
+            }
+        }
         static void Main(string[] args)
         {
-            Game game = new Game();
-
-            while(true)
-            {
-                game.Process();
-            }
+            Map map = new Map();
+            map.Render();
         }
     }
 }
